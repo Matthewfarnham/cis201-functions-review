847994cadc1a7363c479c3eeda52c745351461a6
Matthew Farnham <matthewfarnham@cis.vvc.edu>
2018-10-16 19:59:11 -0700

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
-#include<iostream>
+/*
+File Name: main.cpp
+Author: Matthew Farnham
+Description: This program will prompt the user for any kind of input and then will return the amount of 'a' characters the user entered, both captial and lowercase letters.
+Email: matthef6009@student.vvc.edu
+Date created: Tue Oct 16 18:37:58 PDT 2018
+*/
 
+
+#include<iostream>
+#include<string>
 
 using namespace std;
+
+int count_A(string s)
+{
+    int count = 0;
+    for (int i = 0; i < s.size(); i++)
+    {
+        char c = tolower( s.at(i) );
+        if(c == 'a')
+        {
+            count++;
+        }
+    }
+    if(count >= 10)
+            cout << "Wow, that's a lot of a's!" << endl;
+            
+    return count;
+ }
+
 
 
 int main()
 {
-    cout << "Hello World"<< endl;
-
+    string input;
+    cout << "Say something!" << endl;
+    getline(cin, input);
+    cout << "There are a total of " << count_A(input) << " letter a's in the string you entered." << endl;
     return 0;
 }
